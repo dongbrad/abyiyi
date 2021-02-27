5efe4965c5f831727d22745804be059e7c0c792a
za-dongqingsong <dongqingsong@zhongan.io>
2021-02-27 22:06:09 +0800

    init-1

---
+++ src/main/java/com/abyiyi/AbyiyiApplication.java
@@ -0,0 +1,13 @@
+package com.abyiyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbyiyiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbyiyiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abyiyi/AbyiyiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abyiyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbyiyiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
